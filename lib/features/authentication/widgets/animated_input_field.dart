a24b549561e113d78429b6f2be12acb194534896
$@#il <sahilsadekar249775@gmail.com>
2025-03-25 17:04:54 +0530

    auth finalizeed

--- lib/features/authentication/widgets/animated_input_field.dart
+++ lib/features/authentication/widgets/animated_input_field.dart
@@ -14,7 +14,8 @@ class AnimatedInputField extends StatelessWidget {
   final int maxLines;
   final bool obscureText;
   final VoidCallback? onTogglePassword;
-  final bool enabled; // Add this line
+  final bool enabled;
+  final String? Function(String?)? validator; // Add custom validator parameter
 
   const AnimatedInputField({
     Key? key,
@@ -28,7 +29,8 @@ class AnimatedInputField extends StatelessWidget {
     this.maxLines = 1,
     this.obscureText = false,
     this.onTogglePassword,
-    this.enabled = true, // Add this line with default value
+    this.enabled = true,
+    this.validator, // Add validator to constructor
   }) : super(key: key);
 
   @override
@@ -142,6 +144,11 @@ class AnimatedInputField extends StatelessWidget {
       );
 
   String? _validator(String? value) {
+    // Use custom validator if provided, otherwise use default validation
+    if (validator != null) {
+      return validator!(value);
+    }
+    
     if (value == null || value.isEmpty) {
       return 'This field is required';
     }
