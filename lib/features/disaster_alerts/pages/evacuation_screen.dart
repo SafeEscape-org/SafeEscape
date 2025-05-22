98c998534992c003fb2ec9f7183fdce5040c7c06
$@#il <sahilsadekar249775@gmail.com>
2025-05-22 21:58:36 +0530

    build issues resolve

--- lib/features/disaster_alerts/pages/evacuation_screen.dart
+++ lib/features/disaster_alerts/pages/evacuation_screen.dart
@@ -18,6 +18,7 @@ import 'package:disaster_management/shared/widgets/chat_assistance.dart';
 import '../models/place_type.dart';
 import 'dart:typed_data';
 import 'dart:ui' as ui;
+//cicd checks
 class EvacuationScreen extends StatefulWidget {
   const EvacuationScreen({super.key});
   @override
